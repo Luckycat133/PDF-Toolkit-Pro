4415e9df91286555af80a21a5fa3c9ec765ff026
Luckycat133 <xingmiao201081@gmail.com>
2026-07-18 19:40:31 +0800

    fix: declare Vite URL asset imports

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,4 +1,11 @@
+/// <reference types="vite/client" />
+
 declare module '*.css' {
   const content: Record<string, string>;
   export default content;
+}
+
+declare module '*?url' {
+  const url: string;
+  export default url;
 }
